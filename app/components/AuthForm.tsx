2d48ac7c938bf88e3b0300d48c82e13473d9b40f
Rishu Kumar <rishukr0123@gmail.com>
2023-05-14 11:51:20 +0530

    sign in page made frontend

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import Button from "@/components/Button";
 import Inputs from "@/components/inputs/Inputs";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import AuthSocialButton from "./AuthSocialButton";
+import { BsGithub, BsGoogle  } from 'react-icons/bs';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -54,12 +57,93 @@ const AuthForm = () => {
             className="space-y-6" 
             onSubmit={handleSubmit(onSubmit)}
             >
-                <Inputs label="Email" 
-                    id="email"
-                    register={register}
-                />
+            {variant === 'REGISTER' && (
+            <Inputs
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id="name" 
+              label="Name"
+            />
+          )}
+          <Inputs
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="email" 
+            label="Email address" 
+            type="email"
+          />
+          <Inputs
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id="password" 
+            label="Password" 
+            type="password"
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
 
+            </Button>
+          </div>
             </form>
+            <div className="mt-6">
+          <div className="relative">
+            <div 
+              className="
+                absolute 
+                inset-0 
+                flex 
+                items-center
+              "
+            >
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton 
+              icon={BsGithub} 
+              onClick={() => socialAction('github')} 
+            />
+            <AuthSocialButton 
+              icon={BsGoogle} 
+              onClick={() => socialAction('google')} 
+            />
+          </div>
+        </div>
+        <div 
+          className="
+            flex 
+            gap-2 
+            justify-center 
+            text-sm 
+            mt-6 
+            px-2 
+            text-gray-500
+          "
+        >
+          <div>
+            {variant === 'LOGIN' ? 'New to Messenger?' : 'Already have an account?'} 
+          </div>
+          <div 
+            onClick={toggleVariant} 
+            className="underline cursor-pointer"
+          >
+            {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+          </div>
+        </div>
+        
         </div>
     </div>
   )
