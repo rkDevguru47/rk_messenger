631a519736a40a0db0d0ffcf15d196dc327b5237
Rishu Kumar <rishukr0123@gmail.com>
2023-05-14 14:00:23 +0530

    axios working

--- app/components/AuthForm.tsx
+++ app/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle  } from 'react-icons/bs';
+import axios from "axios";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -37,7 +38,7 @@ const AuthForm = () => {
         setIsLoading(true);
       
         if (variant === 'REGISTER') {
-            //axios register 
+           axios.post('/api/register', data)
         }
     
         if (variant === 'LOGIN') {
@@ -143,7 +144,7 @@ const AuthForm = () => {
             {variant === 'LOGIN' ? 'Create an account' : 'Login'}
           </div>
         </div>
-        
+
         </div>
     </div>
   )
